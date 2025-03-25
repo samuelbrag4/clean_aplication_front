4fab17303674c6b53577faea7d942b57a680aeb0
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2025-03-25 16:26:29 -0300

    feat: adjust

---
+++ src/app/article_skincare/page.jsx
@@ -0,0 +1,10 @@
+import Header from '../components/header';
+
+export default function ArtigoSkincare() {
+    return (
+        <div>
+            <Header corHeader={"#DBBD9C"}/>
+            <h1>texto</h1>
+        </div>
+    );
+};
