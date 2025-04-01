ba8548ea7eac554fd9d5bafde85057c0d624234b
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2025-04-01 14:04:34 -0300

    feat: Adjustments to the category_card component

--- src/app/components/category_card/index.jsx
+++ src/app/components/category_card/index.jsx
@@ -8,22 +8,28 @@ const CategoryCard = () => {
             <nav className={styles.cards}>
                 <Link href="/skincare" className={styles.navLink}>
                     <div className={styles.card}>
-                        <h1 className={styles.tituloCard}>Página de Skincare</h1>
-                        <h2 className={styles.numberCard}> 1 </h2>
+                        <div className={styles.titulo}>
+                            <h1 className={styles.tituloCard}>Página de Skincare</h1>
+                            <h2 className={styles.numberCard}> 1 </h2>
+                        </div>
                         <p className={styles.cardTexto}>O que não pode faltar no skincare? Os passos básicos de uma rotina de skincare simples são: higienização, tonificação, hidratação e proteção solar.</p>
                     </div>
                 </Link>
                 <Link href="/body" className={styles.navLink}>
                     <div className={styles.card}>
-                        <h1 className={styles.tituloCard}>Página de Corpo</h1>
-                        <h2 className={styles.numberCard}> 2 </h2>
+                        <div className={styles.titulo}>
+                            <h1 className={styles.tituloCard}>Página de Corpo</h1>
+                            <h2 className={styles.numberCard}> 2 </h2>
+                        </div>
                         <p className={styles.cardTexto}>O que não pode faltar no skincare? Os passos básicos de uma rotina de skincare simples são: higienização, tonificação, hidratação e proteção solar.</p>
                     </div>
                 </Link >
                 <Link href="/makeup" className={styles.navLink}>
                     <div className={styles.card}>
-                        <h1 className={styles.tituloCard}>Página de Maquiagem</h1>
-                        <h2 className={styles.numberCard}> 3 </h2>
+                        <div className={styles.titulo}>
+                            <h1 className={styles.tituloCard}>Página de Maquiagem</h1>
+                            <h2 className={styles.numberCard}> 3 </h2>
+                        </div>
                         <p className={styles.cardTexto}>O que não pode faltar no skincare? Os passos básicos de uma rotina de skincare simples são: higienização, tonificação, hidratação e proteção solar.</p>
                     </div>
                 </Link>
